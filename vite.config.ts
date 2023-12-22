c93ad4aa2ab936b2dc1e5b92e1f271d3a88332b5
Stavros Kaparelos <skaparelos@gmail.com>
2023-12-22 12:42:01 +0200

    added conditional ssr based on mode. this is for pnpm run dev & pnpm run build to work

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import ssr from "vite-plugin-ssr/plugin";
-import ssr from 'vike/plugin'
+import ssr from "vite-plugin-ssr/plugin";
+import { ssr as vikeSSR } from 'vike/plugin';
+
 import vercel from 'vite-plugin-vercel'
 import { vavite } from "vavite";
 
-export default defineConfig({
-	buildSteps: [
-		{ name: "client" },
-		{
-			name: "server",
-			config: {
-				build: { ssr: true },
+export default defineConfig(({ mode }) => {
+	console.log("mode=", mode)
+	return {
+		buildSteps: [
+			{ name: "client" },
+			{
+				name: "server",
+				config: {
+					build: { ssr: true },
+				},
 			},
+		],
+		server: {
+			port: 4001,
+			strictPort: true,
 		},
-	],
 
-	plugins: [
-		vavite({
-			serverEntry: "/server/index.ts",
-			serveClientAssetsInDev: true,
-		}),
-		react(),
-		ssr({ prerender: true }),
-		vercel()
-	],
+		plugins: [
+			vavite({
+				serverEntry: "/server/index.ts",
+				serveClientAssetsInDev: true,
+			}),
+			react(),
+			mode == 'development' ? ssr({ prerender: true }) : vikeSSR({ prerender: true }),
+			vercel()
+		],
+	}
 });
